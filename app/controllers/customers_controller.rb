bc05c3a3c1c6c94911a34146b6ffbf68f96f6f5e
Indrek Ots <ots.indrek@gmail.com>
2012-10-27 22:20:55 +0300

    datepicker, customer addition

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -11,4 +11,23 @@ class CustomersController < ApplicationController
   def new
   	@customer = Customer.new
   end
+
+  def create
+  	customer = Customer.new
+  	#Rails.logger.info params[:firstName]
+  	customer.firstName = params[:firstName]
+  	customer.lastName = params[:lastName]
+    customer.identityCode = params[:identityCode]
+    customer.cstType = params[:cstType]
+    customer.birthDate = params[:birthDate]
+
+    Rails.logger.info "ajee"
+  	Rails.logger.info customer.firstName
+    Rails.logger.info customer.lastName
+    Rails.logger.info customer.identityCode
+    Rails.logger.info customer.cstType
+    Rails.logger.info customer.birthDate
+
+  	redirect_to customers_path
+  end
 end
