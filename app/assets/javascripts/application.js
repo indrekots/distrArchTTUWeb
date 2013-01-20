587b320d25bc3112d54d762fea4deaa1cce308c6
Indrek Ots <ots.indrek@gmail.com>
2013-01-20 19:28:32 +0200

    alert error added to app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,11 @@ $(document).ready(function () {
 		$('#notices').append("<div class='alert alert-success'><button type='button' class='close' data-dismiss='alert'>×</button><strong>" + data["head"] + "</strong> " + data["body"] + "</div>");
 	})
 
+	$('#add_customer, #add_contract, #update_contract, #update_customer, #add_address, #update_address').
+	bind('ajax:failure', function(xhr, data, status) {
+		$('#notices').append("<div class='alert alert-error'><button type='button' class='close' data-dismiss='alert'>×</button><strong>" + data["head"] + "</strong> " + data["body"] + "</div>");
+	})
+
 	$('.delete_contract, .delete_customer, .delete_address').
 	bind('ajax:success', function(xhr, data, status) {
 		$('#notices').append("<div class='alert alert-success'><button type='button' class='close' data-dismiss='alert'>×</button><strong>" + data["head"] + "</strong> " + data["body"] + "</div>");
